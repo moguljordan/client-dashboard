fe799493b62186b48daac496c13a816bf297e255
Jordan Cobb <moguljordan23@Jordans-MacBook-Pro.local>
2025-08-21 17:02:05 -0400

    added env.local file to hide api

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbw6AQXlo1bTWoSP4x0FUqpbPHt8Xcv8M",
-  authDomain: "mda-database.firebaseapp.com",
-  projectId: "mda-database",
-  storageBucket: "mda-database.appspot.com",
-  messagingSenderId: "289133861766",
-  appId: "1:289133861766:web:b504683d404f153e382a07",
-  measurementId: "G-PH60BGBS9C"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
