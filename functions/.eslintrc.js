de7daee9871e423b72d8bb15ca3ab57b840db1f3
Jordan Cobb <moguljordan23@Jordans-MacBook-Pro.local>
2025-08-22 17:57:38 -0400

    disable no-explicit-any rule for now

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -29,5 +29,7 @@ module.exports = {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "@typescript-eslint/no-explicit-any": "off", // ✅ Disable the "Unexpected any" rule
   },
 };
+
